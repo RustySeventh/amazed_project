357176b65f5de9235fb1dcaae8953c2f3579fae2
Andre <andre.colin@epitech.eu>
2025-04-06 21:50:53 +0200

    on going parsing functions

--- main.c
+++ main.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "rooms.h"
+#include "string.h"
 
-int main(int argc, char **argv) {
+int main() {
     char *line = NULL;
     ssize_t read;
+    size_t size = 0;
+    char *content = NULL;
     while ((read = getline(&line, &size, stdin)) != -1) {
-        printf("%s", line);
+        content = (char *)realloc(content, sizeof(content) + size);
+        strcat(content, line);
     }
-    
+    parse_file(content);
     return 0;
 }
--- parse.c
+++ parse.c
@@ -1,2 +1,21 @@
-#include "room.h"
+#include "rooms.h"
+#include <bits/types/siginfo_t.h>
+#include <stdbool.h>
+#include <string.h>
 
+room_t *parse_file(char *content) {
+    room_t* start = (room_t*)malloc(sizeof(room_t));
+    char *strToken = strtok(content, "\n");
+    // printf("%s", strToken);
+    start->robot = atoi(strToken);
+    while(strToken != NULL) {
+        printf("%s", strToken);
+        if(strcmp(strToken, "##start") == 0) {
+            start->start = 1;
+        }   
+        strToken = strtok(NULL, "\n");
+    }
+    printf("innit\n");
+    // printf("%d\n", start->start);
+    return start;
+}
